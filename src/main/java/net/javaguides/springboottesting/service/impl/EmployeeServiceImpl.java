f9d0568132409a0d7d82503b05508aeb5ff6f31b
Björn Thalheim <bjoern.thalheim@gmx.de>
2023-04-05 10:39:15 +0200

    Added employee service bean.

---
+++ src/main/java/net/javaguides/springboottesting/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboottesting.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import net.javaguides.springboottesting.model.Employee;
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import net.javaguides.springboottesting.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    @Override
+    public Employee save(Employee employee) {
+        return repository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Employee> getById(Long id) {
+        return repository.findById(id);
+    }
+}
